df01a9d6b9dca86dd2222ecca7735d87998b8018
mobus <sv0220@163.com>
2021-12-05 14:18:56 +0800

    add grace exit service

---
+++ grace/service.go
@@ -0,0 +1,39 @@
+package grace
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type serv struct {
+	stop   chan os.Signal
+	cancel func()
+}
+
+type Service interface {
+	Wait()
+}
+
+func (s *serv) Wait() {
+	defer signal.Stop(s.stop)
+	select {
+	case <-s.stop:
+		s.cancel()
+		time.Sleep(100 * time.Millisecond)
+		log.Println("all services exited totally.")
+	}
+}
+
+func New(ctx context.Context) (context.Context, Service) {
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	ctxLocal, cancel := context.WithCancel(ctx)
+	return ctxLocal, &serv{
+		stop:   stopChan,
+		cancel: cancel,
+	}
+}
